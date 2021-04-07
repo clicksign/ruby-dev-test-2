a1ed6819c87158c48f715078978dcd2733deca66
darlandieterich <darlandieterich@gmail.com>
2021-04-06 21:46:49 -0300

    down version to remove table album_players

--- db/migrate/20210406020641_create_album_players.rb
+++ db/migrate/20210406020641_create_album_players.rb
@@ -1,9 +1,13 @@
 class CreateAlbumPlayers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :album_players do |t|
       t.references :album, foreign_key: true
       t.references :player, foreign_key: true
       t.timestamps
     end
+  end
+
+  def down
+    drop_table :album_players
   end
 end
