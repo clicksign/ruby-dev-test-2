9793aed47f4e2663d602e4a8ffb13993a174c872
darlandieterich <darlandieterich@gmail.com>
2021-04-06 22:41:31 -0300

    Schema ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_042611) do
+ActiveRecord::Schema.define(version: 2021_04_07_004309) do
 
   create_table "album_players", force: :cascade do |t|
     t.integer "album_id"
@@ -23,10 +23,8 @@ ActiveRecord::Schema.define(version: 2021_04_06_042611) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
-    t.integer "player_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["player_id"], name: "index_albums_on_player_id"
   end
 
   create_table "players", force: :cascade do |t|
