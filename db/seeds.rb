671d36073a0ab3eafc65803cbb980ac0af5f22c7
darlandieterich <darlandieterich@gmail.com>
2021-04-06 23:20:19 -0300

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+album_ride = Album.create(name: 'Ride the lightning')
+album_master = Album.create(name: 'Master of puppets')
+player = Player.create(name: 'Metallica')
+
+player.albums << album_ride
+player.albums << album_master
