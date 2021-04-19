1bde92a38a887a8df16b5dce2997631e17e3cf2f
Daniel Faiole <danfaiole@gmail.com>
2021-04-19 19:13:21 -0300

    Fixed the migration

--- db/migrate/20210418142139_create_player_albums.rb
+++ db/migrate/20210418142139_create_player_albums.rb
@@ -1,8 +1,8 @@
 class CreatePlayerAlbums < ActiveRecord::Migration[5.2]
   def change
     create_table :player_albums do |t|
-      t.player :references
-      t.album :references
+      t.references :player, foreign_key: true
+      t.references :album, foreign_key: true
 
       t.timestamps
     end
