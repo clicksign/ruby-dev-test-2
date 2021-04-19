0517e105bd738a1b767b2571bfb58f77dfdc6e27
Daniel Faiole <danfaiole@gmail.com>
2021-04-19 19:22:13 -0300

    added dependent destroy to destroy relation

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
-  has_many :player_albums
+  has_many :player_albums, dependent: :destroy
   has_many :players, through: :player_albums
 
   validates_presence_of :name
