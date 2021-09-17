f798cfae91c910cf4e725ac02cd52e40797a1ca5
Amanda <kimberlypenido@gmail.com>
2021-09-17 18:43:11 -0300

    create registers in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating players in database..."
+artist1 = Player.find_or_create_by(name: "Madonna")
+artist2 = Player.find_or_create_by(name: "Shakira")
+
+
+puts "Creating albums in database..."
+album1 = Album.find_or_create_by(name: "American Life", player_id: {artists: [artist1]})
+album2 = Album.find_or_create_by(name: "She Wolf", player_id: {artists: [artist2]})
