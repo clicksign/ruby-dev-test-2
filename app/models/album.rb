c07ec4460875e27a7f87e39a916445c1b85a8105
Amanda <kimberlypenido@gmail.com>
2021-09-17 18:38:41 -0300

    Create method to register split_albums

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,20 @@ class Album < ApplicationRecord
   has_many :players, through: :split_albums, class_name: 'Player'
 
   validates :name, :player_id, presence: {message: "Cannot be blank"}
+
+  after_create :register_split_album
+
+
+  private
+
+  def register_split_album
+    split_album_players = self.player_id
+    player_ids = []
+    split_album_players['artists'].each do |split_album_player|
+      player_ids << split_album_player['id']
+    end
+    player_ids.each do |player_id|
+      SplitAlbum.create(albums_id: self.id, players_id: player_id)
+    end
+  end
 end
