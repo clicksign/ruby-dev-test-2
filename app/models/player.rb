5799f3173356ea1f4c9e9da9cff67fabd6d34329
Amanda <kimberlypenido@gmail.com>
2021-09-17 18:21:15 -0300

    add validation message to class attributes

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,5 +2,5 @@ class Player < ApplicationRecord
   has_many :split_albums, class_name: 'SplitAlbum', foreign_key: 'players_id'
   has_many :albums, through: :split_albums, class_name: 'Album'
 
-  validates_presence_of :name
+  validates :name, presence: {message: "Cannot be blank"}
 end
