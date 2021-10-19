86523859d7e0582acb67da6cebb6644c45aaecba
João Almeida <joao.almeida@corretagemfacil.com.br>
2021-10-18 23:07:01 -0300

    Test 2 ClickSign

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-  has_many :albums
+  has_and_belongs_to_many :albums
 
   validates_presence_of :name
 end
---
+++ db/migrate/20211019015657_create_players_albums_join_table.rb
@@ -0,0 +1,8 @@
+class CreatePlayersAlbumsJoinTable < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :players, :albums do |t|
+      # t.index [:player_id, :album_id]
+      # t.index [:album_id, :player_id]
+    end
+  end
+end
