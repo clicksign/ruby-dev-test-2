61f29eaa71740f04f5a304e2365745d8a1cc90e6
Vítor Ribas <bsbribas66@gmail.com>
2021-10-22 18:40:05 -0300

    feat: create AlbumPlayer join model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,7 @@
 class Album < ApplicationRecord
-  belongs_to :player
+  belongs_to :player  # TO-DO: remove Player references on Album
+  has_many :partnerships, foreign_key: 'album_id', class_name: 'AlbumPlayer'
+  has_many :players, through: :partnerships
 
   validates_presence_of :name
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,7 @@
 class Player < ApplicationRecord
-  has_many :albums
+  has_many :albums # TO-DO: remove Player references on Album
+  has_many :partnerships, foreign_key: 'player_id', class_name: 'AlbumPlayer'
+  has_many :albums, through: :partnerships
 
   validates_presence_of :name
 end
