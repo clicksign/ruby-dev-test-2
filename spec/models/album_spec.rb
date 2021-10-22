384a79a6fc70390339d1c7ff5acd7b878f6e6e05
Vítor Ribas <bsbribas66@gmail.com>
2021-10-22 18:40:55 -0300

    test: add coverage to relationship refactoring

---
+++ spec/models/album_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Album, type: :model do
+  describe 'Album class' do
+    it { should validate_presence_of(:name) }
+    it { should belong_to(:player)} # TO-DO: remove Player references on Album
+    it { should have_many(:partnerships).with_foreign_key('album_id') }
+  end
+end
---
+++ spec/models/player_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Player, type: :model do
+  describe 'Player class' do
+    it { should validate_presence_of(:name) }
+    it { should have_many(:partnerships).with_foreign_key('player_id') }
+  end
+end
