ae10e40879ae47d7053becaf74aac63f76117819
franciscognclvs <fgp1991@gmail.com>
2021-10-26 21:35:24 -0300

    added tests

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = Album.new(name: 'Peligro')
     assert album.valid?
   end
 
@@ -12,9 +12,11 @@ class AlbumTest < ActiveSupport::TestCase
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
-    album = Album.new
-    assert_not album.valid?
-    assert_not_empty album.errors[:player]
+  test "presence of players" do
+    album = Album.new(name: 'She Wolf')
+    album.players << Player.new(name: 'Shakira')
+    album.players << Player.new(name: 'Madonna')
+    assert album.valid?
+    assert_not_empty album.players
   end
 end
