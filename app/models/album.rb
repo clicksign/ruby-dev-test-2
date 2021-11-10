466acfcf8f3ab3b39f6d9c8dfee0499c5a8a2579
kandebonfim <kandebonfim@gmail.com>
2021-11-10 11:45:41 -0300

    Reset development to initial state

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ApplicationRecord
-  has_and_belongs_to_many :players
+  belongs_to :player
 
   validates_presence_of :name
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-  has_and_belongs_to_many :albums
+  has_many :albums
 
   validates_presence_of :name
 end
