a05d692183e1f5532059537c62c9d8912bc91007
Andrew Braga <andrewbraga1@gmail.com>
2021-11-13 13:08:25 -0300

    *Continuação dos testes* - adição de casos; - reescritura dos testes existentes;

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,17 +1,13 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
+ 
   def setup
     @album = Album.new(name: 'Peligro', player: players(:shakira))
   end
+
   test "valid album" do
     assert @album.valid?
-  end
-
-  test 'invalid without name' do
-    @album.name = nil
-    refute @album.valid?, 'saved user without a name'
-    assert_not_nil @album.errors[:name], 'no validation error for name present'
   end
 
   test "presence of name" do
@@ -19,6 +15,18 @@ class AlbumTest < ActiveSupport::TestCase
     assert_not @album.valid?
     assert_not_nil @album.errors[:name]
   end
+
+  test 'invalid without name' do
+    @album.name = nil
+    refute @album.valid?, 'saved album without a name'
+    assert_not_nil @album.errors[:name], 'no validation error for name present'
+  end
+
+  test "should not save album without name" do
+    @album.name = nil
+    assert_not @album.save, "Saved the album without a name"
+  end
+
 
   test "presence of player" do
     @album.player = nil 
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,14 +1,32 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
+  
+  def setup
+    @player = Player.new(name: 'Madonna')
+  end
+
+
   test "valid player" do
-    player = Player.new(name: 'Madonna')
-    assert player.valid?
+    assert @player.valid?
   end
 
   test "presence of name" do
-    player = Player.new
-    assert_not player.valid?
-    assert_not_empty player.errors[:name]
+    @player.name = nil
+    assert_not @player.valid?
+    assert_not_empty @player.errors[:name]
   end
+
+  test 'invalid without name' do
+    @player.name = nil
+    refute @player.valid?, 'saved player without a name'
+    assert_not_nil @player.errors[:name], 'no validation error for name present'
+  end
+
+  test "should not save player without name" do
+    @player.name = nil
+    assert_not @player.save, "Saved the player without a name"
+  end
+
+
 end
