a0b65a066b1218b669eac8def5ea089e0aa6fa1a
Heitor <heitorbelem21@gmail.com>
2021-11-30 14:23:31 -0300

    ♻️ Changed migration method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_30_161529) do
+ActiveRecord::Schema.define(version: 2021_11_30_172207) do
 
   create_table "album_players", force: :cascade do |t|
     t.integer "album_id", null: false
