a881d15c331c3fc9daf7fb9737498434499b1b1f
Arlisson Jaques <arlissonjaques@gmail.com>
2022-02-17 08:21:08 -0300

    fix relationship name

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
   has_many :record_companies
-  has_many :players, through: :record_company
+  has_many :players, through: :record_companies
 
   validates_presence_of :name
 end
