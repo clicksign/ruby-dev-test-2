0586b46b8a5233726713082a91f2474fedf670f3
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2022-05-15 13:18:16 -0300

    feat(test/models): update tests on player_test.rb

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
+  def setup
+    @shakira = players(:shakira)
+    @beyonce = players(:beyonce)
+    @she_wolf = albums(:fixation)
+  end
+
   test "valid player" do
     player = Player.new(name: 'Madonna')
     assert player.valid?
@@ -10,5 +16,14 @@ class PlayerTest < ActiveSupport::TestCase
     player = Player.new
     assert_not player.valid?
     assert_not_empty player.errors[:name]
+  end
+
+  test "associations" do
+    @shakira.albums << @she_wolf
+    @beyonce.albums << @she_wolf
+
+    assert_equal 1, @shakira.albums.size
+    assert_equal "She Wolf",  @beyonce.albums.first.name
+    assert_equal 2,  @she_wolf.players.size
   end
 end
