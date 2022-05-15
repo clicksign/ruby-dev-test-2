1e51e20a8684cc042b7958af48513db890994a6f
Lucas Araujo <lucas.trabalho.araujo@gmail.com>
2022-05-15 13:18:06 -0300

    feat(test/models): update tests on album_test.rb

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
+  def setup
+    @album = albums(:valid)
+    @by_the_way_album = albums(:by_the_way)
+    @flea = players(:flea)
+    @anthony = players(:anthony)
+  end
+
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = Album.new(name: 'Peligro')
     assert album.valid?
   end
 
@@ -12,9 +19,12 @@ class AlbumTest < ActiveSupport::TestCase
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
-    album = Album.new
-    assert_not album.valid?
-    assert_not_empty album.errors[:player]
+  test "associations" do
+    @album.players << @anthony
+    @album.players << @flea
+    @by_the_way_album.players.create(name: "Shakira")
+
+    assert_equal 2, @album.players.size
+    assert_equal "Shakira", @by_the_way_album.players.first.name
   end
 end
