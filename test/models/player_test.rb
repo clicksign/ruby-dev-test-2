86c6305730be7524b5d020ba05c6681189d7c7ef
Bergson Lima <bergsonsud@gmail.com>
2022-05-27 16:06:12 -0300

    test(model): updated tests of player

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
+  def setup
+    @madonna = players(:madonna)
+    @shakira = players(:shakira)
+    @album = albums(:the_best)
+  end
+
   test "valid player" do
-    player = Player.new(name: 'Madonna')
-    assert player.valid?
+    assert @shakira.valid?
   end
 
   test "presence of name" do
     player = Player.new
     assert_not player.valid?
     assert_not_empty player.errors[:name]
+  end
+
+
+  test "associations" do
+    assert_equal 1, @madonna.albums.size
+    assert_equal 2, @album.players.size
   end
 end
