b786485b0e5b46a4419e633faa8679bc26887280
LeandroDosSantosPereira <leandro.pereira@novaandradina.org>
2022-06-04 13:18:53 -0400

    review(formatting)

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,19 +1,18 @@
-require 'test_helper'
+require "test_helper"
 
 class AlbumTest < ActiveSupport::TestCase
-	def setup
-		@album = Album.new(name: 'Peligro')
-	end
+  def setup
+    @album = Album.new(name: "Peligro")
+  end
 
-	test "valid album" do
-		@album.players << players(:shakira)
-		assert @album.valid?
-	end
+  test "valid album" do
+    @album.players << players(:shakira)
+    assert @album.valid?
+  end
 
-
-	test "without presence name" do
-		@album.name = nil
-		assert_not @album.valid?
-		assert_not_empty @album.errors[:name]
-	end
+  test "without presence name" do
+    @album.name = nil
+    assert_not @album.valid?
+    assert_not_empty @album.errors[:name]
+  end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,17 +1,17 @@
-require 'test_helper'
+require "test_helper"
 
 class PlayerTest < ActiveSupport::TestCase
-	def setup
-		@player = Player.new(name: 'Madonna')
-	end
+  def setup
+    @player = Player.new(name: "Madonna")
+  end
 
-	test "valid player" do
-		assert @player.valid?
-	end
+  test "valid player" do
+    assert @player.valid?
+  end
 
-	test "without presence name" do
-		@player.name = nil 
-		assert_not @player.valid?
-		assert_not_empty @player.errors[:name]
-	end
+  test "without presence name" do
+    @player.name = nil
+    assert_not @player.valid?
+    assert_not_empty @player.errors[:name]
+  end
 end
