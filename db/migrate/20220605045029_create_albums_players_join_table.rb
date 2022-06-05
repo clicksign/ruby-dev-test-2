aa0056e3bad747fa608b23b27a0c54a8db09f18b
Jafer Daltro Pompeu Jr <jafer.daltro@gmail.com>
2022-06-05 01:59:12 -0300

    configure many to many relation

---
+++ db/migrate/20220605045029_create_albums_players_join_table.rb
@@ -0,0 +1,8 @@
+class CreateAlbumsPlayersJoinTable < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :albums, :players do |t|
+      t.index :album_id
+      t.index :player_id
+    end
+  end
+end
