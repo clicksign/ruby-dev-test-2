7da78ad66bd99eaeba68fe28951b5b4edefca067
Jafer Daltro Pompeu Jr <jafer.daltro@gmail.com>
2022-06-07 11:04:33 -0300

    fix destination page when success

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,7 +6,7 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
     if @player.save
-      redirect_to albums_path
+      redirect_to root_path
     else
       render 'new'
     end
--- spec/features/players_spec.rb
+++ spec/features/players_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature "Players", type: :feature do
   context 'create new player' do
-    scenario "should redirect page with success" do
+    scenario "should redirect main page when success" do
       visit new_player_path
       within('form') do
         fill_in with: 'Madonna'
       end
       click_button 'Create Player'
-      expect(page).to have_current_path(albums_path) 
+      expect(page).to have_current_path(root_path) 
     end
   end
 end
