3fbae8de5e71bd1d633db62255c886782c10ec91
Leandro Lasnor <leandro.lasnor@mesainc.com.br>
2022-06-29 19:45:24 -0300

    test: changing relationship to NxN

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
+
+  test "has many players" do
+    album = Album.new
+    player = Player.create(name: 'test')
+    album.name = 'test' 
+    album.players << player
+    assert album.valid?
+    assert album.save!
+    assert album.players.count == 1
+  end
+
   test "valid album" do
     album = Album.new(name: 'Peligro', player: players(:shakira))
+    assert album.valid?
+    album = Album.new(name: 'Peligro', player: nil)
     assert album.valid?
   end
 
@@ -10,11 +23,5 @@ class AlbumTest < ActiveSupport::TestCase
     album = Album.new
     assert_not album.valid?
     assert_not_empty album.errors[:name]
-  end
-
-  test "presence of player" do
-    album = Album.new
-    assert_not album.valid?
-    assert_not_empty album.errors[:player]
   end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
+  test "has many albums" do
+    player = Player.new
+    album = Album.new(name: 'test')
+    assert album.valid?
+    album.save!
+    player.name = 'test' 
+    player.albums << album
+    assert player.valid?
+    assert player.save!
+    assert player.albums.count == 1
+  end
+
   test "valid player" do
     player = Player.new(name: 'Madonna')
     assert player.valid?
