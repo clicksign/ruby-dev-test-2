e2960aecc6567419fb3fae892a7e27c65aa0515a
ItaloMennaSilveira <italo.silveira@nave.rs>
2022-07-11 22:25:39 -0300

    feat: add relation many to many between player and album

---
+++ db/migrate/20220712012002_create_join_table_player_album.rb
@@ -0,0 +1,8 @@
+class CreateJoinTablePlayerAlbum < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :players, :albums do |t|
+      t.index :player_id
+      t.index :album_id
+    end
+  end
+end
