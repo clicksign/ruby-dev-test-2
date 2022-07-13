50a0ead7e710f8397ed6c8f2835871442539a0b0
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-13 12:21:22 -0300

    Refatora testes para se adequar as alterações

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,19 +2,27 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = build(:valid_album)
     assert album.valid?
   end
 
   test "presence of name" do
-    album = Album.new
+    album = build(:album_without_name)
     assert_not album.valid?
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
-    album = Album.new
-    assert_not album.valid?
-    assert_not_empty album.errors[:player]
+  test "presence of at least one player" do
+    album = build(:album_without_players)
+    album.save
+    assert_not_empty album.errors[:players]
+  end
+
+  test "saving players relations" do
+    players = [create(:valid_player), create(:valid_player)]
+    album = create(:valid_album, players: players)
+
+    album_saved = Album.find(album.id)
+    assert_equal(players.size, album_saved.players.count)
   end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -2,13 +2,26 @@ require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
   test "valid player" do
-    player = Player.new(name: 'Madonna')
+    player = build(:valid_player)
     assert player.valid?
   end
 
   test "presence of name" do
-    player = Player.new
+    player = build(:player_without_name)
     assert_not player.valid?
     assert_not_empty player.errors[:name]
+  end
+
+  test "recover correct albums" do
+    player1 = create(:valid_player)
+    player2 = create(:valid_player)
+
+    album = create(:valid_album, players: [player1, player2])
+
+    player1_saved = Player.find(player1.id)
+    player2_saved = Player.find(player2.id)
+
+    assert_includes(player1_saved.albums, album)
+    assert_includes(player2_saved.albums, album)
   end
 end
