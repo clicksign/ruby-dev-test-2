3b85bf9a3bc92f6be7ee8cedd0ea76cec74df2f0
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-13 10:55:00 -0300

    Altera models de players e albums para terem relação many-to-many

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
-  belongs_to :player
+  has_and_belongs_to_many :players
 
   validates_presence_of :name
+  validates :players, association_count: { minimum: 1 }
 end
