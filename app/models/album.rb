6c0a44b2b6da9de7cd406c651a999b6ca6cbf526
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-07-13 12:20:35 -0300

    Ajusta validações dos models

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
   has_and_belongs_to_many :players
 
-  validates_presence_of :name
-  validates :players, association_count: { minimum: 1 }
+  validates :name, presence: true, allow_blank: false
+  validates :players, length: { minimum: 1 }
 end
