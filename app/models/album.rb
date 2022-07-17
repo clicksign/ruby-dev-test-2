902d2d0419b9504d6938c92e6ad97feb000a3ec7
Frederico Linhares <fred@linhares.blue>
2022-07-17 09:22:21 -0300

    Change relationship of albums and players to many-to-many

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,10 @@
 class Album < ApplicationRecord
-  belongs_to :player
+  has_and_belongs_to_many :players
 
   validates_presence_of :name
+  validate :has_one_or_more_players
+
+  def has_one_or_more_players
+    errors.add(:players, "need one or more players") if players.empty?
+  end
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = Album.new(name: 'Peligro', players: [players(:shakira)])
     assert album.valid?
   end
 
@@ -15,6 +15,15 @@ class AlbumTest < ActiveSupport::TestCase
   test "presence of player" do
     album = Album.new
     assert_not album.valid?
-    assert_not_empty album.errors[:player]
+    assert_not_empty album.errors[:players]
+  end
+
+  test "player can have multiple albums" do
+    shakira = players(:shakira)
+    madona = Player.new(name: "Madona")
+    album = Album.new(name: "New Album", players: [shakira, madona])
+
+    assert album.save!
+    assert_equal 2, album.players.size
   end
 end
