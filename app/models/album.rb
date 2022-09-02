dc0eb25bb5eaa2c4144b8277885c9be516a38bf6
Allan Escóssio <allanjohnson.pe@gmail.com>
2022-09-01 21:55:29 -0300

    feat: Add players presence validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,13 @@
 class Album < ApplicationRecord
-  belongs_to :player
+  has_and_belongs_to_many :players
 
   validates_presence_of :name
+
+  validate :has_players
+
+  private
+
+  def has_players
+    errors.add(:players, "needs at least one player") if players.empty?
+  end
 end
