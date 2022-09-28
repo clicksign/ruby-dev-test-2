daa28f1b5aac3eb4cb3dc4e6be5d7bcc57104a1e
Otho TK <otho.tk@hotmail.com>
2022-09-27 22:31:43 -0300

    🏗️ DB settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_203647) do
+ActiveRecord::Schema.define(version: 2022_09_27_015835) do
 
-  create_table "albums", force: :cascade do |t|
+  create_table "albums", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.integer "player_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["player_id"], name: "index_albums_on_player_id"
   end
 
-  create_table "players", force: :cascade do |t|
+  create_table "albums_players", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "album_id"
+    t.bigint "player_id"
+    t.index ["album_id"], name: "index_albums_players_on_album_id"
+    t.index ["player_id"], name: "index_albums_players_on_player_id"
+  end
+
+  create_table "players", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
