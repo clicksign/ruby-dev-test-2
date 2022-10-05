ceba3e9f95a1c9adab9a0f8b0cc7f648983adb31
Pedro Gabriel <eeevee@gmail.com>
2022-10-05 14:43:05 -0300

    chore: add down data handler on migration

--- db/migrate/20221005123649_create_album_players_join_table.rb
+++ db/migrate/20221005123649_create_album_players_join_table.rb
@@ -5,13 +5,16 @@ class CreateAlbumPlayersJoinTable < ActiveRecord::Migration[5.2]
       t.index :player_id
     end
 
-    Album.reset_column_information
     Album.find_each do |album|
       album.update!(players: [album.player]) if album.player
     end
   end
 
   def down
+    Album.find_each do |album|
+      album.update!(player: album.players.first) if album.players.length
+    end
+
     drop_join_table :albums, :players
   end
 end
