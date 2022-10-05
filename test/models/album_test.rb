61f7cd16fdf99e4770591dbe6e9865e50399bf7e
Pedro Gabriel <eeevee@gmail.com>
2022-10-05 10:36:15 -0300

    test: fix and add tests to new relation in album

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    players = [players(:shakira), players(:beyonce)]
+    album = Album.new(name: 'Peligro', players: players)
+    assert album.players.map(&:name).contains(['Shakira', 'Beyonce'])
     assert album.valid?
   end
 
@@ -12,9 +14,9 @@ class AlbumTest < ActiveSupport::TestCase
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
+  test "presence of players" do
     album = Album.new
     assert_not album.valid?
-    assert_not_empty album.errors[:player]
+    assert_empty album.errors[:players]
   end
 end
