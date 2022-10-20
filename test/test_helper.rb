d1ca75ec4d083b926854e93d6ce90f6c0f36dab4
Horecio Araujo Dias <horecio@gmail.com>
2022-10-20 23:30:49 +0000

    chore: add shoulda matchers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f }
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
