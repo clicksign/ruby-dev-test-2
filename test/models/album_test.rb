18d973427d65f68d75426b54d6435c7e85b64721
Caio Vitor Santos <caio.souza@guide.com.br>
2022-11-02 21:35:59 -0300

    Test albums associations many-to-many

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,19 +2,22 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = albums(:fijacion)
     assert album.valid?
+  end
+
+  test "valid album associations" do
+    album = albums(:fijacion)
+    album_players(:fijacion_beyonce)
+    album_players(:fijacion_shakira)
+
+    assert album.valid?
+    assert_equal 2, album.players.count
   end
 
   test "presence of name" do
     album = Album.new
     assert_not album.valid?
     assert_not_empty album.errors[:name]
-  end
-
-  test "presence of player" do
-    album = Album.new
-    assert_not album.valid?
-    assert_not_empty album.errors[:player]
   end
 end
