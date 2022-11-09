64fcb8cba4d10a43eb6bd38335885c8fef9ca72c
José Antonio <juninho.juze@gmail.com>
2022-11-08 23:47:34 -0300

    (+) Validate association presence and update tests

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,19 +2,24 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = albums(:virgin)
+
     assert album.valid?
   end
 
   test "presence of name" do
-    album = Album.new
+    album = albums(:virgin)
+    album.name = nil
+
     assert_not album.valid?
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
-    album = Album.new
+  test "presence of players" do
+    album = albums(:virgin)
+    album.players = []
+
     assert_not album.valid?
-    assert_not_empty album.errors[:player]
+    assert_not_empty album.errors[:players]
   end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -2,13 +2,24 @@ require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
   test "valid player" do
-    player = Player.new(name: 'Madonna')
+    player = players(:shakira)
+
     assert player.valid?
   end
 
   test "presence of name" do
-    player = Player.new
+    player = players(:shakira)
+    player.name = nil
+
     assert_not player.valid?
     assert_not_empty player.errors[:name]
+  end
+
+  test "presence of albums" do
+    player = players(:shakira)
+    player.albums = []
+
+    assert_not player.valid?
+    assert_not_empty player.errors[:albums]
   end
 end
