11f068808390e7a24942afb99d52d6a16c6dc352
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-11-11 02:25:56 -0300

    chore: set fixtures for tests

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,9 +2,11 @@ class Album < ApplicationRecord
   has_many :albums_players
   has_many :players, through: :albums_players
 
+  # Remove after migrations
   def self.columns
     super.reject { |c| c.name == "player_id" }
   end
+  # Remove after migrations
 
   validates_presence_of :name, :players
 end
