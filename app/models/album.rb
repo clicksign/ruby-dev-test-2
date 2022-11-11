d2dc409899222c1534d785b326d1c2f37a3a2bb5
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2022-11-11 02:12:10 -0300

    feat: add migrations to remove player_id column from albums table

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,5 +2,9 @@ class Album < ApplicationRecord
   has_many :albums_players
   has_many :players, through: :albums_players
 
+  def self.columns
+    super.reject { |c| c.name == "player_id" }
+  end
+
   validates_presence_of :name, :players
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_032421) do
+ActiveRecord::Schema.define(version: 2022_11_11_034224) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
