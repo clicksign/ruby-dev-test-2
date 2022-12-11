14ecbb626b2fa10a3979db3f95f12b1c44ab0e9b
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-12-11 00:26:36 -0300

    Fix shoulda-matchers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,17 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'shoulda/matchers'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :minitest
+      with.library :rails
+    end
+  end
   # Add more helper methods to be used by all tests here...
 end
