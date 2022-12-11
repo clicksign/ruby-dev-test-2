fbdba2345d7d459cdedcc30c502eef3b72fb54dd
lpaivareis <lucasfelipedepaiva2@gmail.com>
2022-12-10 23:55:23 -0300

    Add new table to relationship album_players

---
+++ db/migrate/20221211024101_create_album_players.rb
@@ -0,0 +1,8 @@
+class CreateAlbumPlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :album_players do |t|
+      t.references :album, foreign_key: true
+      t.references :player, foreign_key: true
+    end
+  end
+end
