488b151fa2caa576f7bcaffe251ea7da7d239ba3
Paulo Tada <paulohtfs@gmail.com>
2023-06-15 11:12:09 -0300

    feat: fixing and incrementing tests

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = Album.new(name: 'Peligro', players: [players(:shakira)])
     assert album.valid?
   end
 
@@ -12,9 +12,9 @@ class AlbumTest < ActiveSupport::TestCase
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
-    album = Album.new
-    assert_not album.valid?
-    assert_not_empty album.errors[:player]
+  test "players association" do
+    albums_players = [players(:shakira), players(:beyonce)]
+    album = Album.new(name: 'Beautiful Liar', players: albums_players)
+    assert_not_empty album.players
   end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -11,4 +11,10 @@ class PlayerTest < ActiveSupport::TestCase
     assert_not player.valid?
     assert_not_empty player.errors[:name]
   end
+
+  test "album association" do
+    players_albums = [albums(:fijacion), albums(:she_wolf)]
+    player = Player.new(name: 'Shakira', albums: players_albums)
+    assert_not_empty player.albums
+  end
 end
