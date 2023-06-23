9f23f8b1bb05dccc90fce3e97b19c6a356b3eabb
Augusto Stroligo <gutostroligo@gmail.com>
2023-06-23 20:41:40 -0300

    chore: added tests and fixtures for habtm relationship

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
   test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+    album = albums(:fijacion)
     assert album.valid?
   end
 
@@ -12,9 +12,23 @@ class AlbumTest < ActiveSupport::TestCase
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
-    album = Album.new
-    assert_not album.valid?
-    assert_not_empty album.errors[:player]
+  test "albums can belong to multiple players" do
+    shakira = players(:shakira)
+    madonna = players(:madonna)
+    album = albums(:ray_of_light)
+
+    assert_equal 2, album.players.size
+    assert_includes album.players, shakira
+    assert_includes album.players, madonna
+  end
+
+  test "players can have multiple albums" do
+    shakira = players(:shakira)
+    ray_of_light = albums(:ray_of_light)
+    fixation = albums(:fixation)
+
+    assert_operator shakira.albums.size, :>=, 2
+    assert_includes shakira.albums, ray_of_light
+    assert_includes shakira.albums, fixation
   end
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
   test "valid player" do
-    player = Player.new(name: 'Madonna')
+    player = players(:beyonce)
     assert player.valid?
   end
 
@@ -10,5 +10,15 @@ class PlayerTest < ActiveSupport::TestCase
     player = Player.new
     assert_not player.valid?
     assert_not_empty player.errors[:name]
+  end
+
+  test "players can have multiple albums" do
+    player = players(:shakira)
+    album1 = albums(:fijacion)
+    album2 = albums(:fixation)
+
+    assert_operator player.albums.size, :>=, 2
+    assert_includes player.albums, album1
+    assert_includes player.albums, album2
   end
 end
