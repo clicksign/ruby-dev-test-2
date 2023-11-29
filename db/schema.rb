f6f845f0cc7fdc102a70d499380a66fb1316f5e5
Renan Cadore Aguiar <cadore@cashway.io>
2023-11-29 20:13:03 -0300

    Cria tabela de junção, altera relacionamentos nos models e testes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_02_203647) do
+ActiveRecord::Schema.define(version: 2023_11_29_224215) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_08_02_203647) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["player_id"], name: "index_albums_on_player_id"
+  end
+
+  create_table "albums_players", id: false, force: :cascade do |t|
+    t.integer "album_id", null: false
+    t.integer "player_id", null: false
+    t.index ["album_id", "player_id"], name: "index_albums_players_on_album_id_and_player_id"
+    t.index ["player_id", "album_id"], name: "index_albums_players_on_player_id_and_album_id"
   end
 
   create_table "players", force: :cascade do |t|
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,20 +1,35 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+  test 'valid album' do
+    album = Album.new(
+      name: 'Peligro',
+      players: [players(:shakira), players(:beyonce)]
+    )
     assert album.valid?
   end
 
-  test "presence of name" do
+  test 'presence of name' do
     album = Album.new
     assert_not album.valid?
     assert_not_empty album.errors[:name]
   end
 
-  test "presence of player" do
-    album = Album.new
+  test 'presence of players' do
+    album = Album.new(name: 'Album')
     assert_not album.valid?
-    assert_not_empty album.errors[:player]
+    assert_not_empty album.errors[:players]
+  end
+
+  test 'should have and belongs to many players' do
+    released = albums(:released)
+    shakira = players(:shakira)
+    beyonce = players(:beyonce)
+
+    assert_includes released.players, shakira
+    assert_includes released.players, beyonce
+
+    assert_includes shakira.albums, released
+    assert_includes beyonce.albums, released
   end
 end
